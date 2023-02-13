ab66a51f848aa2014bdba7dfa3dc4f87f81d417c
root <lmslmsms0616@gmail.com>
2023-02-13 12:11:08 +0900

    Update WebConfig.java

--- src/main/java/Forcloud/chat/config/WebConfig.java
+++ src/main/java/Forcloud/chat/config/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry){
         // TODO: allowedOrigins-> 우리 서버 ip로 수정 필요 ex)http://192.126.32.2:3000
